323c0b9a92a533d6a8684a0a349b30b82de8635a
Arne Zeising <arne.zeising@gmail.com>
2018-09-07 13:43:26 +0200

    Add optional title attribute to TextField

--- src/field/TextField.js
+++ src/field/TextField.js
@@ -4,19 +4,26 @@ import get from 'lodash/get';
 import pure from 'recompose/pure';
 import sanitizeRestProps from './sanitizeRestProps';
 
-const TextField = ({ className, source, record = {}, ...rest }) => {
+const TextField = ({ addTitle, className, source, record = {}, ...rest }) => {
+    const value = get(record, source)
+
     return (
-        <span className={className} {...sanitizeRestProps(rest)}>
-            {get(record, source)}
+        <span
+            className={className}
+            title={addTitle ? value : undefined}
+            {...sanitizeRestProps(rest)}
+        >
+            {value}
         </span>
     );
 };
 
 TextField.propTypes = {
     addLabel: PropTypes.bool,
+    addTitle: PropTypes.bool,
     basePath: PropTypes.string,
-    className: PropTypes.string,
     cellClassName: PropTypes.string,
+    className: PropTypes.string,
     headerClassName: PropTypes.string,
     label: PropTypes.string,
     record: PropTypes.object,
@@ -27,6 +34,7 @@ const PureTextField = pure(TextField);
 
 PureTextField.defaultProps = {
     addLabel: true,
+    addTitle: false
 };
 
 export default PureTextField;
